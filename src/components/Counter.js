ae8df92644d9564b6c43fa20122bf20981d21c6e
iShivam2012 <shivam.gupta97a@gmail.com>
2021-07-06 00:25:32 +0530

    dispatched actions from Counter and used useSelector to fetch the slice of an updated state

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,29 @@
-import classes from './Counter.module.css';
+import { useDispatch, useSelector } from "react-redux";
+import classes from "./Counter.module.css";
 
 const Counter = () => {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
   const toggleCounterHandler = () => {};
+  const incrementHandler = () => {
+    dispatch({
+      type: "INC",
+    });
+  };
+  const decrementHandler = () => {
+    dispatch({
+      type: "DEC",
+    });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
+      <button onClick={incrementHandler}>Increment</button>
+      <button onClick={decrementHandler}>Decrement</button>
     </main>
   );
 };
